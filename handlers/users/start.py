9e19675d9c092cf09b5ae1a0ad6c04af0adebc2d
helloworldrussia <coolgrifers@gmail.com>
2022-07-30 13:01:03 +0300

    first commit1

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,32 +1,10 @@
-import mysql.connector
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
-from data.config import start_text
-from data.connector import connection
-from keyboards.default.buttons import markup
 from loader import dp, bot
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    writer_id = message.from_user.id
-    writer_name = message.from_user.full_name
-
-    await bot.send_photo(chat_id=writer_id, photo='https://ibb.co/R0TXCkH',
-                         reply_markup=markup)
-    await bot.send_message(chat_id=writer_id, text=start_text)
-
-    conn = connection()
-    cursor = conn.cursor()
-
-    cursor.execute(f"SELECT * FROM users_table WHERE user_id = {writer_id}")
-    resault = cursor.fetchone()
-
-    if resault is None:
-        cursor.execute(f"INSERT INTO users_table(user_id, user_name, user_level) VALUES('{writer_id}', '{writer_name}', '1')")
-        conn.commit()
-    else:
-        pass
-    print(writer_id)
-    conn.close()
+    await bot.send_photo(chat_id=message.from_user.id,
+                         photo='https://jtele2.ru/wp-content/uploads/2019/03/tele2.jpg')
